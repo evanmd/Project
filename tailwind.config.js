ab4c8b9db0ae28c479bb96d550ac48f736dec332
Evan Dinger <evanmdinger@gmail.com>
2025-05-01 18:10:39 -0400

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./public/**/*.html",
+      "./src/**/*.{js,ts}", // include your JS/TS files in src/
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
