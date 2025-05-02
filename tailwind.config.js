802bc7b2ee56c27fea3bc45233d23b3939e1cb6d
Evan Dinger <evanmdinger@gmail.com>
2025-05-02 08:19:58 -0400

    Updated 6 files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-      "./public/**/*.html",
-      "./src/**/*.{js,ts}", // include your JS/TS files in src/
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
